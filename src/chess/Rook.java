f8df6c691b87a32762b2ff7c9d08ebde9e9c2440
NickBurkland <nickburkland5@gmail.com>
2015-11-20 22:40:41 -0700

    Added rook

---
+++ src/chess/Rook.java
@@ -0,0 +1,17 @@
+package chess;
+
+public class Rook extends GamePeice {
+
+	@Override
+	public void move(Location l) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Location[] getMovements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
