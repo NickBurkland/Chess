4deec97615c48212a9cafa28db952dceb95819b0
Sage Smith <sagersmith8@gmail.com>
2015-11-21 00:50:49 -0700

    fixed Location

--- src/chess/Location.java
+++ src/chess/Location.java
@@ -1,23 +1,32 @@
 package chess;
 
 public class Location {
-    char x = 'A';
+    int x = 0;
     int y = 0;
     
     public Location(char x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+	this.x = (int)(x-'A');
+	this.y = y;
+    }
 
-	public Location shift(char x, int y){
-    	return new Location((char)(x+((char)3)), this.y+y);
+    public Location(int x, int y){
+	this.x = x;
+	this.y = y;
+    }
+
+    public Location shift(char x, int y){
+    	return new Location((char)this.x+x, this.y+y);
+    }
+
+    public Location shift(int x, int y){
+	return new Location(this.x+x,this.y+y);
     }
 	
-	public int getX() {
-		return (int)x;
-	}
+    public int getX() {
+	return x;
+    }
 	
-	public int getY() {
-		return y;
-	}
+    public int getY() {
+	return y;
+    }
 }
