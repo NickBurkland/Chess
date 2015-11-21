14deb17713d3900f18d65fddfbd525520724a2f9
NickBurkland <nickburkland5@gmail.com>
2015-11-20 22:38:32 -0700

    Minor changes to Game

--- src/chess/Game.java
+++ src/chess/Game.java
@@ -3,12 +3,16 @@ package chess;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class Game extends JPanel implements ActionListener {
 
 	GamePeice[][] peices = new GamePeice[8][8];
+	JButton[][] board = new JButton[8][8];
 	String name;
+	
+	
 	
 	public Game(String name) {
 		
