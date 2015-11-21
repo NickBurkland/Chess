52d50c3bc4644500a4c4ec3915f9ac9e84ee0966
NickBurkland <nickburkland5@gmail.com>
2015-11-20 22:52:47 -0700

    Fixed IllegalMoveException

---
+++ src/chess/Rook.java
@@ -0,0 +1,20 @@
+package chess;
+
+import java.util.ArrayList;
+
+public class Rook extends GamePeice {
+
+	public Rook(Location location, Team team) {
+		super(location, team);
+		
+	}
+	
+	public void move(Location l) {
+
+	}
+
+	public ArrayList<Location> getMovements() {
+		return null;
+	}
+
+}
