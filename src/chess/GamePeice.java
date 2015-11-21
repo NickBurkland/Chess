c9c988e0485c87c604ac1e51a6fe4dddec348d5b
Sage Smith <sagersmith8@gmail.com>
2015-11-20 22:44:14 -0700

    Started on pawn

--- src/chess/GamePeice.java
+++ src/chess/GamePeice.java
@@ -1,25 +1,31 @@
 package chess;
 
 public abstract class GamePeice {
-	protected Location location;
-	protected boolean alive = true;
-	protected Team team;
+    protected Location location;
+    protected boolean alive = true;
+    protected Team team;
+    protected String image;
 	
-	public GamePeice(Location location, Team team) {
-		super();
-		this.location = location;
-		this.team = team;
-	}
+    public GamePeice(Location location, Team team, String image) {
+	super();
+	this.location = location;
+	this.team = team;
+	this.image = image;
+    }
 	
-	public abstract void move(Location l);
+    public abstract void move(Location l);
 	
-	public abstract Location[] getMovements();
+    public abstract Location[] getMovements();
 	
-	public Team getTeam() {
-		return team;
-	}
+    public Team getTeam() {
+	return team;
+    }
 	
-	public void kill() {
-		alive = false;
-	}
+    public void kill() {
+	alive = false;
+    }
+
+    public String getImage(){
+	return image;
+    }
 }
---
+++ src/chess/Pawn.java
@@ -0,0 +1,38 @@
+package chess;
+
+public class Pawn extends GamePiece{
+    public Pawn(Location location, Team team){
+	super(location, team, "P");
+    }
+
+    public ArrayList<Location> getMovements(){
+	switch(team){
+	    ArrayList<Location> locations = new ArrayList<Location>();
+	case 0:
+	    if(location.y==1){
+		locations.add(location.shift(0,2));
+		locations.add(location.shift(0,1));
+	    }
+	    else{
+		locations.add(location.shift(0,1));
+	    }
+	    break;
+	case 1:
+	    if(location.y==6){
+		locations.add(location.shift(0,-2));
+		locations.add(location.shift(0,-1));
+	    }
+	    else{
+		locations.add(location.shift(0,-1));
+	    }
+	    break;
+	}    
+    }
+    
+    public void move(Location location){
+	if(getMovements().contains(location))
+	    this.location = location;
+	else
+	    throw new IllegalMoveException();
+    }
+}
