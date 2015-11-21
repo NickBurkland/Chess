92d31e10c86c3c2c9e3a355685a81673578d8f52
NickBurkland <nickburkland5@gmail.com>
2015-11-21 14:33:48 -0700

    fixed pieces

--- src/chess/Game.java
+++ src/chess/Game.java
@@ -31,30 +31,30 @@ public class Game extends JPanel implements ActionListener {
 			}
 		}
 
-		peices[0][0]=new Rook(new Location(0,0),Team.WHITE, peices);
-		peices[7][0]=new Rook(new Location(7,0),Team.WHITE, peices);
-		peices[0][7]=new Rook(new Location(0,7),Team.BLACK, peices);
-		peices[7][7]=new Rook(new Location(7,7),Team.BLACK, peices);
+		pieces[0][0]=new Rook(new Location(0,0),Team.WHITE, pieces);
+		pieces[7][0]=new Rook(new Location(7,0),Team.WHITE, pieces);
+		pieces[0][7]=new Rook(new Location(0,7),Team.BLACK, pieces);
+		pieces[7][7]=new Rook(new Location(7,7),Team.BLACK, pieces);
 		
-		peices[1][0]=new Knight(new Location(1,0),Team.WHITE, peices);
-		peices[6][0]=new Knight(new Location(6,0),Team.WHITE, peices);
-		peices[1][7]=new Knight(new Location(1,7),Team.BLACK, peices);
-		peices[6][7]=new Knight(new Location(6,7),Team.BLACK, peices);
+		pieces[1][0]=new Knight(new Location(1,0),Team.WHITE, pieces);
+		pieces[6][0]=new Knight(new Location(6,0),Team.WHITE, pieces);
+		pieces[1][7]=new Knight(new Location(1,7),Team.BLACK, pieces);
+		pieces[6][7]=new Knight(new Location(6,7),Team.BLACK, pieces);
 		
-		peices[2][0]=new Bishop(new Location(2,0),Team.WHITE, peices);
-		peices[5][0]=new Bishop(new Location(5,0),Team.WHITE, peices);
-		peices[2][7]=new Bishop(new Location(2,7),Team.BLACK, peices);
-		peices[5][7]=new Bishop(new Location(5,7),Team.BLACK, peices);
+		pieces[2][0]=new Bishop(new Location(2,0),Team.WHITE, pieces);
+		pieces[5][0]=new Bishop(new Location(5,0),Team.WHITE, pieces);
+		pieces[2][7]=new Bishop(new Location(2,7),Team.BLACK, pieces);
+		pieces[5][7]=new Bishop(new Location(5,7),Team.BLACK, pieces);
 		
-		peices[3][0]=new Queen(new Location(3,0),Team.WHITE, peices);
-		peices[3][7]=new Queen(new Location(3,7),Team.BLACK, peices);
+		pieces[3][0]=new Queen(new Location(3,0),Team.WHITE, pieces);
+		pieces[3][7]=new Queen(new Location(3,7),Team.BLACK, pieces);
 		
-		peices[4][0]=new King(new Location(4,0),Team.WHITE, peices);
-		peices[4][7]=new King(new Location(4,7),Team.BLACK, peices);
+		pieces[4][0]=new King(new Location(4,0),Team.WHITE, pieces);
+		pieces[4][7]=new King(new Location(4,7),Team.BLACK, pieces);
 		
-		for(int x = 0; x < peices.length; x++){
-			peices[x][1]= new Pawn(new Location(x,1),Team.WHITE, peices);
-			peices[x][6]= new Pawn(new Location(x,6),Team.BLACK, peices);
+		for(int x = 0; x < pieces.length; x++){
+			pieces[x][1]= new Pawn(new Location(x,1),Team.WHITE, pieces);
+			pieces[x][6]= new Pawn(new Location(x,6),Team.BLACK, pieces);
 		}			
 	}
 	
