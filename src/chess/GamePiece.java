22281bc70625cfdbb0318ff77085c434d3d8ba04
Sage Smith <sagersmith8@gmail.com>
2015-11-21 14:20:14 -0700

    Got rid of is alive and Kill in gamepeice

--- src/chess/GamePiece.java
+++ src/chess/GamePiece.java
@@ -4,41 +4,36 @@ import java.util.ArrayList;
 
 public abstract class GamePiece {
 
-protected Location location;
-    protected boolean alive = true;
-    protected Team team;
-    protected String image;
-    protected Team opp;
+	protected Location location;
+	protected Team team;
+	protected String image;
+	protected Team opp;
+	protected GamePiece[][] pieces;
 
-    protected GamePiece[][] pieces;
-	
-    public GamePiece(Location location, Team team, String image, GamePiece[][] pieces) {
+	public GamePiece(Location location, Team team, String image, GamePiece[][] pieces) {
 		super();
 		this.location = location;
 		this.team = team;
 		this.image = image;
 		this.pieces = pieces;
-		
+
 		if(this.team == Team.BLACK)
-		    this.opp = Team.WHITE;
+			this.opp = Team.WHITE;
 		else
-		    this.opp = Team.BLACK;
+			this.opp = Team.BLACK;
 
-    }
-	
-    public abstract void move(Location l);
-	
-    public abstract ArrayList<Location> getMovements();
-	
-    public Team getTeam() {
-	return team;
-    }
-	
-    public void kill() {
-	alive = false;
-    }
+	}
 
-    public String getImage(){
-	return image;
-    }
+	public abstract void move(Location l);
+
+	public abstract ArrayList<Location> getMovements();
+
+	public Team getTeam() {
+		return team;
+	}
+
+
+	public String getImage(){
+		return image;
+	}
 }
